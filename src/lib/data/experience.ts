f49e03663f714160e230719ce765be72e771ec2c
Copy of Mavis's Brain <58226449+Mavis-H@users.noreply.github.com>
2024-10-08 16:34:42 -0700

    sort experience

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -4,24 +4,6 @@ import { ContractType, type Experience } from '../types';
 
 export const items: Array<Experience> = [
 	{
-		slug: 'software-development-intern',
-		company: 'CertiK',
-		description: '- Developed blockchain security monitoring products (<b>Python</b>) with <b>Snowflake, Kafka, Airflow, and Datadog</b> in production.\n' +
-						'- Researched Solana NFT wash-trade detection, applied directed graph cycle detection by detecting SCC, reducing false positive by 20% and increasing detection speed by 10% using batch processing.\n' +
-						'- Managed <b>CI/CD</b> deployment with <b>GitHub, Docker, Terraform, ECS, and AWS Fargate</b>; maintained <b>CI/CD</b> documentation.\n' +
-						'- Performed unit testing on monitors internal logic, conducted integration testing with Kafka data consumption and production to ensure seamless data flow and reliability, and executed system testing to verify correct product deployment on AWS.\n',
-		contract: ContractType.Internship,
-		type: 'Software Development',
-		location: 'Remote',
-		period: { from: new Date(2022, 5, 0), to: new Date(2022, 8, 0) },
-		skills: getSkills('python', 'snowflake', 'kafka', 'redis', 'aws', 'docker', 'terraform', 'datadog', 'git', 'pycharm', 'jira', 'confluence', 'ecs', 'fargate'),
-		name: 'Software Development Intern',
-		color: 'green',
-		links: [{ to: 'https://www.certik.com', label: 'CertiK'}],
-		logo: Assets.CertiK,
-		shortDescription: 'Developing blockchain monitor products.'
-	},
-	{
 		slug: 'fullstack-software-engineer',
 		company: 'ToHacking',
 		description: '- Designed back-end services for data ingestion and storage, improving short-term rental listing configurations.\n' +
@@ -40,7 +22,25 @@ export const items: Array<Experience> = [
 		links: [{ to: '', label: 'ToHacking'}],
 		logo: Assets.ToHacking,
 		shortDescription: 'Developing short-term rental assistant products.'
-	}
+	},
+	{
+		slug: 'software-development-intern',
+		company: 'CertiK',
+		description: '- Developed blockchain security monitoring products (<b>Python</b>) with <b>Snowflake, Kafka, Airflow, and Datadog</b> in production.\n' +
+						'- Researched Solana NFT wash-trade detection, applied directed graph cycle detection by detecting SCC, reducing false positive by 20% and increasing detection speed by 10% using batch processing.\n' +
+						'- Managed <b>CI/CD</b> deployment with <b>GitHub, Docker, Terraform, ECS, and AWS Fargate</b>; maintained <b>CI/CD</b> documentation.\n' +
+						'- Performed unit testing on monitors internal logic, conducted integration testing with Kafka data consumption and production to ensure seamless data flow and reliability, and executed system testing to verify correct product deployment on AWS.\n',
+		contract: ContractType.Internship,
+		type: 'Software Development',
+		location: 'Remote',
+		period: { from: new Date(2022, 5, 0), to: new Date(2022, 8, 0) },
+		skills: getSkills('python', 'snowflake', 'kafka', 'redis', 'aws', 'docker', 'terraform', 'datadog', 'git', 'pycharm', 'jira', 'confluence', 'ecs', 'fargate'),
+		name: 'Software Development Intern',
+		color: 'green',
+		links: [{ to: 'https://www.certik.com', label: 'CertiK'}],
+		logo: Assets.CertiK,
+		shortDescription: 'Developing blockchain monitor products.'
+	},
 ];
 
 export const title = 'Experience';
